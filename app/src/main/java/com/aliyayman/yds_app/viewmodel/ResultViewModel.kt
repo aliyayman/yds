080a43183e235ceee478275ea755f56a6544056c
mac <ali-yayman@hotmail.com>
2024-02-01 15:15:18 +0300

    added Result Screen

---
+++ app/src/main/java/com/aliyayman/yds_app/viewmodel/ResultViewModel.kt
@@ -0,0 +1,25 @@
+package com.aliyayman.yds_app.viewmodel
+
+import android.app.Application
+
+class ResultViewModel(application: Application) : BaseViewModel(application) {
+    fun checkResult(correctNumber: Int): String {
+        return when (correctNumber) {
+            10 -> "You're a genius!"
+            8, 9 -> "You did great!"
+            6, 7 -> "You're doing well!"
+            else -> "You can do better!"
+        }
+    }
+    fun writeMessage(correctNumber: Int) :String{
+        val wrongNumber = 10 - correctNumber
+        return "$correctNumber Correct, $wrongNumber Mistake"
+
+    }
+    fun calculateRate(correctNumber: Int) :String{
+        val rate = correctNumber * 10
+        return "%$rate SUCCESS"
+    }
+
+
+}
