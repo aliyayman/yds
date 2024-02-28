0d051fdf766846264e4b2e759206f9d82a6a956a
mac <ali-yayman@hotmail.com>
2024-02-28 09:37:49 +0300

    add new ad id

--- app/src/main/java/com/aliyayman/yds_app/view/FavoritesFragment.kt
+++ app/src/main/java/com/aliyayman/yds_app/view/FavoritesFragment.kt
@@ -109,8 +109,15 @@ class FavoritesFragment : Fragment(), TextToSpeech.OnInitListener {
         viewModel.getFavoriteWords()
         viewModel.favoriteWords.observe(viewLifecycleOwner, Observer { words ->
             words?.let {
-                binding.recyclerViewWordFav.visibility = View.VISIBLE
-                wordAdapter.differ.submitList(words)
+                if (words.isNotEmpty()){
+                    binding.recyclerViewWordFav.visibility = View.VISIBLE
+                    wordAdapter.differ.submitList(words)
+                    binding.emptyTextView.visibility = View.GONE
+
+                }else{
+                    binding.emptyTextView.visibility = View.VISIBLE
+                }
+
             }
         })
         viewModel.wordError.observe(viewLifecycleOwner, Observer { error ->
