85d431dc70f373fef334a39163334b62fc64f8db
mac <ali-yayman@hotmail.com>
2024-04-02 16:14:30 +0300

    changed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.aliyayman.yds_app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
