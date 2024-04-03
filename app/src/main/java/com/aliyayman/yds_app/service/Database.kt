aace85fab9f00e323f2ae458bbec48a007756bd7
mac <ali-yayman@hotmail.com>
2024-04-03 14:02:42 +0300

    update room version

--- app/src/main/java/com/aliyayman/yds_app/service/Database.kt
+++ app/src/main/java/com/aliyayman/yds_app/service/Database.kt
@@ -8,7 +8,7 @@ import com.aliyayman.yds_app.model.Article
 import com.aliyayman.yds_app.model.Category
 import com.aliyayman.yds_app.model.Word
 
-@Database(entities = [Category::class, Word::class,Article::class], version = 2)
+@Database(entities = [Category::class, Word::class,Article::class], version = 3)
 abstract class myDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun wordDao(): WordDao
