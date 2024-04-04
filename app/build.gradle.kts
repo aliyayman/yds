b36d7c19a164927521466f8036323e5ef694decb
mac <ali-yayman@hotmail.com>
2024-04-04 11:22:42 +0300

    done 1.9 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.aliyayman.yds_app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
